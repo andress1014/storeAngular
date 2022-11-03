836b0c1eda704a4aa06a7ac153e516d06bd3b39e
Andres Garcia M <andres.garcia1014@ehotmail.com>
2022-11-03 14:08:49 -0500

    Proyecto Angulasr

---
+++ src/app/shared/components/header/header.component.ts
@@ -0,0 +1,13 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-header',
+  template: `
+  <mat-toolbar color="primary">
+  <span>Store</span>
+</mat-toolbar>
+`,
+  styleUrls: ['./header.component.sass']
+})
+export class HeaderComponent  {
+}
